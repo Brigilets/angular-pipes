ac8371a91d1350e4a0721978a26b6fd9dfee50d3
Brigilets <b.sabutyte@gmail.com>
2023-08-09 15:38:33 +0200

    added pipe that allows to filter data by the value of the object property & implemented it in the code, changed default behaviour by adding pure:false to the @Pipe decorator to allow display off newly added servers while the filtering is in place

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  //allows to add new server while filter is in place
+  pure: false,
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string, propName: string) {
+    if (value.length === 0 || filterString === '') {
+      return value;
+    }
+    const resultArr = [];
+    for (const item of value) {
+      if (item[propName] === filterString) {
+        resultArr.push(item);
+      }
+    }
+    return resultArr;
+  }
+}
